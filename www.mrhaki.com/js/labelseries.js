b56f7af89ca2078e4b9a224c5b8c4d737e840ec0
Hubert Klein Ikkink <h.kleinikkink@gmail.com>
2014-10-14 13:47:55 +0200

    Added spocklight series.

--- www.mrhaki.com/js/labelseries.js
+++ www.mrhaki.com/js/labelseries.js
@@ -7,8 +7,11 @@ if (/^Groovy Goodness:.*$/.test(blogitemTitle)) {
     blogitemLabel = "Gradle:Goodness";
 } else if (/^Grails Goodness:.*$/.test(blogitemTitle)) {
     blogitemLabel = "Grails:Goodness";
+} else if (/^Awesome Asciidoc:.*$/.test(blogitemTitle)) {
+    blogitemLabel = "Awesome:Asciidoc";
+} else if (/^Spocklight:.*$/.test(blogitemTitle)) {
+    blogitemLabel = "Spocklight";
 }
-
 if (blogitemLabel) {
     var url = "http://pipes.yahoo.com/pipes/pipe.run?_id=d443a3bc3450d40648e6939b0a49bd42&_render=json&label=" + escape(blogitemLabel) + "&title=" + escape(blogitemTitle) + "&_callback=?"
     $.getJSON(url, function(data) {
