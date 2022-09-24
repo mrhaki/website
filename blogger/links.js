30218aef3694f8d6111f4f99718ccc261e197f1d
Hubert A. Klein Ikkink <h.kleinikkink@gmail.com>
2022-09-24 14:02:33 +0200

    Add https for external link

--- blogger/links.js
+++ blogger/links.js
@@ -1,4 +1,4 @@
-<a href="http://www.linkedin.com/in/mrhaki"><img border="0" width="120" alt="View Hubert Klein Ikkink's profile on LinkedIn" src="http://www.linkedin.com/img/webpromo/btn_viewmy_120x33.gif" height="33"/></a>
+<a href="https://www.linkedin.com/in/mrhaki"><img border="0" width="120" alt="View Hubert Klein Ikkink's profile on LinkedIn" src="http://www.linkedin.com/img/webpromo/btn_viewmy_120x33.gif" height="33"/></a>
 <br/>
 <br/>
-<a href="http://mrhaki.deviantart.com/"><img border="0" width="120" src="http://bp1.blogger.com/_-vJw6r2W-bw/R-ecWdmzGeI/AAAAAAAAAuQ/pEARzQoQVOI/s400/DeviantArt_Banner.png"/></a>
+<a href="https://mrhaki.deviantart.com/"><img border="0" width="120" src="https://bp1.blogger.com/_-vJw6r2W-bw/R-ecWdmzGeI/AAAAAAAAAuQ/pEARzQoQVOI/s400/DeviantArt_Banner.png"/></a>
