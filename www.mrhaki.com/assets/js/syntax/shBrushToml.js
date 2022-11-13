df9e608eb0e893c14fdda12cb35072ec6226c420
Hubert A. Klein Ikkink <h.kleinikkink@gmail.com>
2022-11-13 14:07:37 +0100

    Small changes in TOML and YAML brushes

--- www.mrhaki.com/assets/js/syntax/shBrushToml.js
+++ www.mrhaki.com/assets/js/syntax/shBrushToml.js
@@ -32,16 +32,15 @@
      var constants   = '~ true false on off';
  
      this.regexList = [
-        { regex: regexLib.singleLinePerlComments, css: 'comments' },      // comment
-        { regex: regexLib.doubleQuotedString,     css: 'string' },        // double quoted string
-        { regex: regexLib.singleQuotedString,     css: 'string' },        // single quoted string
+        { regex: SyntaxHighlighter.regexLib.singleLinePerlComments, css: 'comments' },      // comment
+        { regex: SyntaxHighlighter.regexLib.doubleQuotedString,     css: 'string' },        // double quoted string
+        { regex: SyntaxHighlighter.regexLib.singleQuotedString,     css: 'string' },        // single quoted string
         { regex: /^\s*([a-z0-9\._-])+\s*:/gmi,                      css: 'variable' },      // key
         { regex: /\s?(\.)([a-z0-9\._-])+\s?:/gmi,                   css: 'comments' },      // section
         { regex: /\s(@|:)([a-z0-9\._-])+\s*$/gmi,                   css: 'variable bold' }, // variable, reference
         { regex: /\s+\d+\s?$/gm,                                    css: 'color2 bold' },   // integers
-        { regex: /(\{|\}|\[|\]|,|~|:)/gm,                           css: 'constants' },     // inline hash and array, comma, null
+        { regex: /(,|~|:)/gm,                           css: 'constants' },     // inline hash and array, comma, null
         { regex: /^\s+(-)+/gm,                                      css: 'string bold' },   // array list entry
-        { regex: /^---/gm,                                          css: 'string bold' },   // category
         { regex: new RegExp(this.getKeywords(constants), 'gmi'),    css: 'constants' }      // constants
     ];
  };
