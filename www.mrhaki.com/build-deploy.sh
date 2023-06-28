881b7ce86734c4e06afae73e93ac9b96a7282a5a
Hubert A. Klein Ikkink <h.kleinikkink@gmail.com>
2023-06-28 16:34:35 +0200

    Add list of Leanpub books

--- www.mrhaki.com/build-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-jbake -b
-rsync -rvuz --progress output/ mrhaki@162.209.73.215:/home/mrhaki/public_html/www.mrhaki.com/public
--- www.mrhaki.com/deploy_cloud.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rsync -rvuz --progress output/ mrhaki@162.209.73.215:/home/mrhaki/public_html/www.mrhaki.com/public
